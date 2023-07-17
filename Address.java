ddf2e68dba67348f5fecc917f4a3c7510eaa200d
maruti <marutigurav978@gmail.com>
2023-07-17 15:13:34 +0530

    Pushed

---
+++ Address.java
@@ -0,0 +1,13 @@
+package com.spring;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Address {
+	
+	public void Data()
+	{
+		System.out.println("i am from Mumbai");
+	}
+
+}
